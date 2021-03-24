131d270d1c19dfeaa6d24cc917e27d547528b63e
Ariful Alam <arif@orangetoolz.com>
2021-03-24 19:49:18 +0600

    Refactor code

--- types/index.d.ts
+++ types/index.d.ts
@@ -10,152 +10,152 @@ export type type = 'button' | 'submit' | 'reset';
 export type size = 'tiny' | 'small' | 'normal' | 'large';
 
 export interface ReactiveButtonProps {
-  /**
-   * Current button state.
-   * 
-   * Default: 'idle'
-   */
-  buttonState?: buttonState;
+    /**
+     * Current button state.
+     * 
+     * Default: 'idle'
+     */
+    buttonState?: buttonState;
 
-  /**
-   * Callback function when clicking button.
-   * 
-   * Default: () => {}
-   */
-  onClick?: (event: React.MouseEvent) => void;
+    /**
+     * Callback function when clicking button.
+     * 
+     * Default: () => {}
+     */
+    onClick?: (event: React.MouseEvent) => void;
 
-  /**
-   * Button color.
-   * 
-   * Default: 'primary'
-   */
-  color?: color;
+    /**
+     * Button color.
+     * 
+     * Default: 'primary'
+     */
+    color?: color;
 
-  /**
-   * Button text when idle.
-   * 
-   * Default: 'Click Me'
-   */
-  idleText?: string | ReactNode;
+    /**
+     * Button text when idle.
+     * 
+     * Default: 'Click Me'
+     */
+    idleText?: string | ReactNode;
 
-  /**
-   * Button text when loading.
-   * 
-   * Default: 'Loading'
-   */
-  loadingText?: string | ReactNode;
+    /**
+     * Button text when loading.
+     * 
+     * Default: 'Loading'
+     */
+    loadingText?: string | ReactNode;
 
-  /**
-   * Button text when loading successful.
-   * 
-   * Default: 'Success'
-   */
-  successText?: string | ReactNode;
+    /**
+     * Button text when loading successful.
+     * 
+     * Default: 'Success'
+     */
+    successText?: string | ReactNode;
 
-  /**
-   * Button text when loading failed.
-   * 
-   * Default: 'Error'
-   */
-  errorText?: string | ReactNode;
+    /**
+     * Button text when loading failed.
+     * 
+     * Default: 'Error'
+     */
+    errorText?: string | ReactNode;
 
-  /**
-   * Button type attribute.
-   * 
-   * Default: 'button'
-   */
-  type?: type;
+    /**
+     * Button type attribute.
+     * 
+     * Default: 'button'
+     */
+    type?: type;
 
-  /**
-   * Button classnames.
-   * 
-   * Default: ''
-   */
-  className?: string;
+    /**
+     * Button classnames.
+     * 
+     * Default: ''
+     */
+    className?: string;
 
-  /**
-   * Custom style.
-   * 
-   * Default: {}
-   */
-  style?: React.CSSProperties;
+    /**
+     * Custom style.
+     * 
+     * Default: {}
+     */
+    style?: React.CSSProperties;
 
-  /**
-   * Enable outline effect.
-   * 
-   * Default: false
-   */
-  outline?: boolean;
+    /**
+     * Enable outline effect.
+     * 
+     * Default: false
+     */
+    outline?: boolean;
 
-  /**
-   * Enable shadow effect.
-   * 
-   * Default: false
-   */
-  shadow?: boolean;
+    /**
+     * Enable shadow effect.
+     * 
+     * Default: false
+     */
+    shadow?: boolean;
 
-  /**
-   * Enable rounded button.
-   * 
-   * Default: false
-   */
-  rounded?: boolean;
+    /**
+     * Enable rounded button.
+     * 
+     * Default: false
+     */
+    rounded?: boolean;
 
-  /**
-   * Button size.
-   * 
-   * Default: 'normal'
-   */
-  size?: size;
+    /**
+     * Button size.
+     * 
+     * Default: 'normal'
+     */
+    size?: size;
 
-  /**
-   * Block button.
-   * 
-   * Default: false
-   */
-  block?: boolean;
+    /**
+     * Block button.
+     * 
+     * Default: false
+     */
+    block?: boolean;
 
-  /**
-   * Success/Error message duration in millisecond.
-   * 
-   * Default: 2000
-   */
-  messageDuration?: number;
+    /**
+     * Success/Error message duration in millisecond.
+     * 
+     * Default: 2000
+     */
+    messageDuration?: number;
 
-  /**
-   * Disable button.
-   * 
-   * Default: false
-   */
-  disabled?: boolean;
+    /**
+     * Disable button.
+     * 
+     * Default: false
+     */
+    disabled?: boolean;
 
-  /**
-   * Button reference.
-   * 
-   * Default null
-   */
-  buttonRef: React.Ref | null;
+    /**
+     * Button reference.
+     * 
+     * Default null
+     */
+    buttonRef: React.Ref | null;
 
-  /**
-   * Override button width.
-   * 
-   * Default null
-   */
-  width: string | null;
+    /**
+     * Override button width.
+     * 
+     * Default null
+     */
+    width: string | null;
 
-  /**
-   * Override button height.
-   * 
-   * Default null
-   */
-  height: string | null;
+    /**
+     * Override button height.
+     * 
+     * Default null
+     */
+    height: string | null;
 
-  /**
-   * Button hover and click animation.
-   * 
-   * Default true
-   */
-  animation: boolean;
+    /**
+     * Button hover and click animation.
+     * 
+     * Default true
+     */
+    animation: boolean;
 }
 
 declare class ReactiveButton extends React.Component<ReactiveButtonProps, any> {}
